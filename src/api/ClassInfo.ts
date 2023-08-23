83dcec0e4e584ab0be631dfec0865c51a39101bd
211250165 <211250165@smail.nju.edu.cn>
2023-08-23 17:45:05 +0800

    update: 实现了第一个api调用

--- src/api/ClassInfo.ts
+++ src/api/ClassInfo.ts
@@ -15,12 +15,21 @@ export async function getClassByTeacher(teacherName: string){
 
 // 数据库根据order返回class
 export async function getClass(order: number): Promise<ClassItem>{
-    return {
-        "id": "11110A",
-        "name": "高等数学",
-        "teacher": "张三",
-        "time": ["周一 1-2节", "周三 3-4节"],
-        "rate": 4.5,
-        "location": "教学楼A101",
+    const baseUrl ="http://127.0.0.1:5000/class/"
+    // return {
+    //     "id": "11110A",
+    //     "name": "高等数学",
+    //     "teacher": "张三",
+    //     "time": ["周一 1-2节", "周三 3-4节"],
+    //     "rate": 4.5,
+    //     "location": "教学楼A101",
+    // }
+    try {
+        const response = await axios.get(baseUrl + order);
+        console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching class:", error);
+        throw error;
     }
 }
